276a6998eaa0f5d8e936dd3233d3979825e77ab1
Ibrahim Kouma <ikouma269@gmail.com>
2021-08-05 15:19:49 +0800

    random shapes

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-const { Engine, Render, Runner, World, Bodies } = Matter;
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
 
 const engine = Engine.create();
-
+const width = 800;
+const height = 600;
 const { world } = engine;
 const render = Render.create({
 	element: document.body,
 	engine: engine,
 	options: {
-		width: 800,
-		height: 600
+		wireframes:false,
+		width,
+		height
 	}
 });
 
 Render.run(render);
 Runner.run(Runner.create(), engine);
+World.add(
+	world,
+	MouseConstraint.create(engine, {
+		mouse: Mouse.create(render.canvas)
+	})
+);
 
-const shapes = Bodies.rectangle(200, 200, 50, 50, {
-	isStatic: true
-});
+//walls
+const walls = [
+	Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+	Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+	Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+	Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+World.add(world, walls);
 
-World.add(world, shapes);
+//Random shapes
+
+for (let i = 0; i < 20; i++) {
+	if (Math.random() > 0.5) {
+			World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
+
+	} else {
+		World.add(world, Bodies.circle(Math.random() * width,Math.random() * height,35,{
+			render:{
+				fillStyle:'red'
+			}
+		}))
+	}
+}
